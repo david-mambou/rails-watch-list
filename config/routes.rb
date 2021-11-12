5a710b6ea925d9414fed945cf724adc561e31938
David Mambou <david.mambou@gmail.com>
2021-11-12 14:00:45 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'lists'
+  root to: 'lists#index'
   get 'lists', to: 'lists#index', as: :lists
   get 'lists/new', to: 'lists#new', as: :new_list
   post 'lists', to: 'lists#create'
