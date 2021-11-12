7673f1b7d3010c35da91900fce4bb1d709e5b326
David Mambou <david.mambou@gmail.com>
2021-11-12 13:40:30 +0900

    remove the at

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists'
   get 'lists', to: 'lists#index', as: :lists
   get 'lists/new', to: 'lists#new', as: :new_list
   post 'lists', to: 'lists#create'
